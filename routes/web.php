fd791229cb7f3adb2e2eff179c6af698ef589364
VitalyZibulski <laravel.vitaly.zibulski2017@gmail.com>
2018-08-19 09:35:55 +0300

    add route, view, home, post

--- routes/web.php
+++ routes/web.php
@@ -1,16 +1,13 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+//site routes
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/','SiteController@showHome');
+Route::get('{slug}','SiteController@showPOst');
+
+
+//auth routes
+
+//subs routes
+
+//account routes
